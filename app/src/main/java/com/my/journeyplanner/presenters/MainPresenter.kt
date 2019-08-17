0ba1efa46e25fb8ae82612e89ccd7352a8f06323
akwc10 <akwc10@sky.com>
2019-08-17 16:57:48 +0100

    Forgot to include MainPresenter in previous commit

--- app/src/main/java/com/my/journeyplanner/presenters/MainPresenter.kt
+++ app/src/main/java/com/my/journeyplanner/presenters/MainPresenter.kt
@@ -1,7 +1,7 @@
 package com.my.journeyplanner.presenters
 
 import android.util.Log
-import com.my.journeyplanner.apiService
+import com.my.journeyplanner.helpers.JourneyPlannerApiService
 import com.my.journeyplanner.models.JourneyPlannerDisambiguationResult
 import com.my.journeyplanner.views.main.MainContract
 import retrofit2.Call
@@ -9,7 +9,9 @@ import retrofit2.Callback
 import retrofit2.Response
 
 class MainPresenter(private val view: MainContract.View) : MainContract.Presenter {
-    override fun onChangTimeClicked() {
+    private var call: Call<JourneyPlannerDisambiguationResult>? = null
+
+    override fun onChangeTimeClicked() {
 
     }
 
@@ -17,13 +19,18 @@ class MainPresenter(private val view: MainContract.View) : MainContract.Presente
 
     }
 
-    override fun onPlanMyJourneyClicked(): Call<JourneyPlannerDisambiguationResult> {
-        val call = apiService.getJourneyResults(
+    override fun onPlanMyJourneyClicked() {
+        val apiService = JourneyPlannerApiService.createApiService
+
+        call = apiService.getJourneyResults(
             view.getFromLocation().text.toString(),
             view.getToLocation().text.toString()
         )
 
-        call.enqueue(object : Callback<JourneyPlannerDisambiguationResult> {
+/*            TODO("Default adapter treats response code 300 Multiple Options as unsuccessful and")
+            TODO("puts response in the errorBody but is actually a valid response.")
+            TODO("Implement custom adapter to parse the valid response in the errorBody")   */
+        call!!.enqueue(object : Callback<JourneyPlannerDisambiguationResult> {
             override fun onFailure(call: Call<JourneyPlannerDisambiguationResult>, throwable: Throwable) {
                 Log.e(TAG, "Other error: ${throwable.message}")
                 Log.e(TAG, "Other error: ${throwable.stackTrace.forEach { println(it) }}")
@@ -40,11 +47,14 @@ class MainPresenter(private val view: MainContract.View) : MainContract.Presente
                 }
             }
         })
-        return call
     }
 
     override fun onMyJourneysClicked() {
 
+    }
+
+    override fun cancelAsyncCall() {
+        call?.cancel()
     }
 
     companion object {
